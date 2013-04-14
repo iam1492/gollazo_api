0667643d89e8ed8ca4fc80a91b18bf34bf2628f8
kai <lovefamilydev@gmail.com>
2013-04-15 00:26:57 +0900

    add reply

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,15 +34,44 @@ class PostsController < ApiController
   end
 
   def add_reply
-	@post = Post.find(params[:id])
-	@comment = @post.comments.build(:content => params[:content],
-                                 :selected_num => params[:selected_num],
-                                 :imei => params[:imei]
-                                 )
-	if @post.save 
-	    render :json => {:success => true, :result_code => 0, :comment => @comment, :message => "succeed to create comment"}
-	else
-	    render :json => {:success => false, :result_code => 2, :comment => @comment.errors, :message => "fail to create comment"}
-	end
+  	@post = Post.find(params[:id])
+    @imei = params[:imei]
+    @selected_num = params[:selected_num]
+    @user = User.getUserInfo(@imei)
+
+    if (@user.nil?)
+      render :json=>{:success => false, :message=>"user is null"}
+      return      
+    end
+
+  	@comment = @post.comments.build(:content => params[:content],
+                                   :selected_num => @selected_num,
+                                   :imei => @imei
+                                   )
+
+    if (!(@user.voted_up_on? @post))   
+    
+      @post.vote(:voter => @user)
+      case @selected_num.to_i
+        when 0
+          @post.vote_count_1 = @post.vote_count_1 + 1
+        when 1
+          @post.vote_count_2 = @post.vote_count_2 + 1
+        when 2
+          @post.vote_count_3 = @post.vote_count_3 + 1
+        when 3
+          @post.vote_count_4 = @post.vote_count_4 + 1
+        else
+          ;
+      end
+    else
+    
+    end
+
+  	if @post.save 
+  	    render :json => {:success => true, :result_code => 0, :comment => @comment, :message => "succeed to create comment"}
+  	else
+  	    render :json => {:success => false, :result_code => 2, :comment => @comment.errors, :message => "fail to create comment"}
+  	end
   end
 end
