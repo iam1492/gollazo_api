6860f10ecfcdac6e6f9af5321009ca8b77f952b7
kai <lovefamilydev@gmail.com>
2013-04-24 23:36:51 +0900

    add voted post api

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,17 @@ class PostsController < ApiController
     end
   end
 
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(params[:board])
+
+    if @post.save
+      render :json=>{:success => true, :result_code => 0, :message=>"success to update post."}
+    else
+      render :json=>{:success => false, :result_code => 2, :message=>"fail to update post."}
+    end
+  end
+
   def show
   	@post = Post.find(params[:id])
     @imei = params[:imei]
@@ -55,6 +66,23 @@ class PostsController < ApiController
 
   def getPostsByImei 
     @posts = Post.where("imei = ?", params[:imei]).page(params[:page]).order('created_at DESC')
+    if (@posts.nil?)
+      render :json=>{:success => false, :message=>"fail to get posts."}
+    else
+      metadata = {:success => true, :message=>"success to get posts.", :posts_count => @posts.count}
+      respond_with(@posts, :api_template => :render_post_list, :root => :posts, :meta => metadata)
+    end
+  end
+
+  def getVotedPosts
+    @imei = params[:imei]
+    @user = User.cachedUserInfo(@imei)
+    if (@user.nil?)
+      @user = User.getUserInfo(@imei);
+    end
+    @votables = @user.find_votes.page(params[:page]).order('created_at DESC')
+    @posts_ids = @votables.map{|post| post.votable_id}
+    @posts = Post.find(@posts_ids)
     if (@posts.nil?)
       render :json=>{:success => false, :message=>"fail to get posts."}
     else
--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,11 @@ GollazoApi::Application.routes.draw do
   match 'users/get_nickname_uniqueness(.format)' => "users#checkUniqueness", :via => :get 
   
   match 'posts(.format)' => "posts#create", :via => :post 
+  match 'posts/update(.format)' => "posts#update", :via => :post
   match 'posts(.format)' => "posts#show", :via => :get
   match 'posts/get_post_by_category(.format)' => "posts#getPostsByCategory", :via => :get
   match 'posts/add_comment(.format)' => "posts#add_reply", :via => :post 
   match 'posts/bomb_post(.format)' => "posts#bombPost", :via => :post
   match 'posts/get_post_by_imei(.format)' => "posts#getPostsByImei", :via => :get
-  
+  match 'posts/get_voted_post_by_imei(.format)' => "posts#getVotedPosts", :via => :get
 end
